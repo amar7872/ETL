d3bea5a53d681a4c65e2003f0c8342534857b555
Amar <Amar7872@gmail.com>
2023-06-06 18:08:19 +0000

    new version

---
+++ config.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from google.cloud import storage, bigquery, secretmanager
+import os
+from pathlib import Path
+import json
+
+GCP_PROJECT_ID = "project-test1-389013"
+
+def get_secret(self, project_id, secret_name, version="latest")-> dict:
+
+    c=secretmanager.SecretManagerServiceClient.from_service_account_json("/home/amar_gcplearning7872/secrets/secretmanager.json")
+    secret_name_long=f"projects/{project_id}/secrets/{secret_name}/versions/{version}"
+    response:str = c.access_secret_version(name=secret_name_long)
+    secret_dict:dict = json.loads(response.payload.data.decode("UTF-8"))   
+    return secret_dict
+
+
+def get_storage_client(self, credentials_dict:dict):
+
+    client=storage.Client.from_service_account_info(credentials_dict)
+
+    return client
+
+
+
+def get_bigquery_client():
+    pass
+
